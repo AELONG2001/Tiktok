a496df32eae0119d7ad307330f9c6683d90d8e72
long 2001 <longkm2708@gmail.com>
2022-05-27 22:33:51 +0700

    dung base header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { DefaultLayout } from '~/components/Layouts';
+import { publicRoutes } from '~/routes';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, idx) => {
+                        const Page = route.component;
+                        
+                        let Layout = DefaultLayout
+
+                        if (route.layout) {
+                            Layout = route.layout
+                        }else if (route.layout === null) {
+                            Layout = Fragment
+                        }
+
+                        return (
+                            <Route
+                                key={idx}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
